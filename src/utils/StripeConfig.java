cb4160c8d8dd362c73cfde9aff8ac530abf2e489
SamirShrestha777 <samirstha4253@gmail.com>
2026-06-14 13:51:42 +0545

    Stripe api added

---
+++ src/utils/StripeConfig.java
@@ -0,0 +1,7 @@
+// utils/StripeConfig.java
+package utils;
+
+public class StripeConfig {
+    public static final String SECRET_KEY = "sk_test_your_key_here";
+    public static final String PUBLISHABLE_KEY = "pk_test_your_key_here";
+}
