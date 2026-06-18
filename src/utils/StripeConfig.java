281c36065bdb57d0a9f116538a748481ada7aeaa
SamirShrestha777 <samirstha4253@gmail.com>
2026-06-18 21:32:48 +0545

    merge issue

--- src/utils/StripeConfig.java
+++ src/utils/StripeConfig.java
@@ -2,6 +2,6 @@
 package utils;
 
 public class StripeConfig {
-    public static final String SECRET_KEY = "sk_test_your_key_here";
-    public static final String PUBLISHABLE_KEY = "pk_test_51Ti8tzQx0XmKaAaxKyMhCVw5Db4eQVvk9QNQ7s1KzgZjIUarm2NXyc6VQWSV4K6G9oayTXKUMJK4QFxfV7MoLwiv00ZJlaoDrm";
+    public static final String SECRET_KEY = "STRIPE_SECRET_KEY";
+    public static final String PUBLISHABLE_KEY = "STRIPE_PUBLISHABLE_KEY";
 }
