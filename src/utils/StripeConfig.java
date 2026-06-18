2cefcb9f23dd4b57727ddf87a5b2b87d2bb49835
SamirShrestha777 <samirstha4253@gmail.com>
2026-06-18 21:28:08 +0545

    Notification card changes made

--- src/utils/StripeConfig.java
+++ src/utils/StripeConfig.java
@@ -3,5 +3,5 @@ package utils;
 
 public class StripeConfig {
     public static final String SECRET_KEY = "sk_test_your_key_here";
-    public static final String PUBLISHABLE_KEY = "pk_test_your_key_here";
+    public static final String PUBLISHABLE_KEY = "pk_test_51Ti8tzQx0XmKaAaxKyMhCVw5Db4eQVvk9QNQ7s1KzgZjIUarm2NXyc6VQWSV4K6G9oayTXKUMJK4QFxfV7MoLwiv00ZJlaoDrm";
 }
